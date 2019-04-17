3595e95c2e2c4ba64760c20606b781780d6f5894
likky <664907696@qq.com>
2019-04-17 15:50:17 +0800

    Initial commit

---
+++ app/src/main/java/com/rain/takephotodemo/MainActivity.java
@@ -0,0 +1,234 @@
+package com.rain.takephotodemo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * 该项目主要用于拍照练习
+ * 适配安卓7.0及以上
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_TAKE_PHOTO = 0;// 拍照
+    private static final int REQUEST_CROP = 1;// 裁剪
+    private static final int SCAN_OPEN_PHONE = 2;// 相册
+    private static final int REQUEST_PERMISSION = 100;
+    private ImageView img;
+    private Uri imgUri; // 拍照时返回的uri
+    private Uri mCutUri;// 图片裁剪时返回的uri
+    private boolean hasPermission = false;
+    private File imgFile;// 拍照保存的图片文件
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_takephoto).setOnClickListener(this);
+        findViewById(R.id.btn_open_photo_album).setOnClickListener(this);
+        img = findViewById(R.id.iv);
+
+        checkPermissions();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_takephoto:
+                checkPermissions();
+                if (hasPermission) {
+                    takePhoto();
+                }
+                break;
+
+            case R.id.btn_open_photo_album:
+                checkPermissions();
+                if (hasPermission) {
+                    openGallery();
+                }
+
+                break;
+
+            default:
+                break;
+
+
+        }
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, SCAN_OPEN_PHONE);
+    }
+
+
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // 检查是否有存储和拍照权限
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                    && checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+            ) {
+                hasPermission = true;
+            } else {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, REQUEST_PERMISSION);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                hasPermission = true;
+            } else {
+                Toast.makeText(this, "权限授予失败！", Toast.LENGTH_SHORT).show();
+                hasPermission = false;
+            }
+        }
+    }
+
+    // 拍照
+    private void takePhoto() {
+        // 要保存的文件名
+        String time = new SimpleDateFormat("yyyyMMddHHmmss", Locale.CHINA).format(new Date());
+        String fileName = "photo_" + time;
+        // 创建一个文件夹
+        String path = Environment.getExternalStorageDirectory() + "/take_photo";
+        File file = new File(path);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        // 要保存的图片文件
+        imgFile = new File(file, fileName + ".jpeg");
+        // 将file转换成uri
+        // 注意7.0及以上与之前获取的uri不一样了，返回的是provider路径
+        imgUri = getUriForFile(this, imgFile);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // 添加Uri读取权限
+        intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+        // 或者
+//        grantUriPermission("com.rain.takephotodemo", imgUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        // 添加图片保存位置
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+        intent.putExtra("return-data", false);
+        startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+    }
+
+    // 图片裁剪
+    private void cropPhoto(Uri uri, boolean fromCapture) {
+        Intent intent = new Intent("com.android.camera.action.CROP"); //打开系统自带的裁剪图片的intent
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+
+        // 注意一定要添加该项权限，否则会提示无法裁剪
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+        intent.putExtra("scale", true);
+
+        // 设置裁剪区域的宽高比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+
+        // 设置裁剪区域的宽度和高度
+        intent.putExtra("outputX", 200);
+        intent.putExtra("outputY", 200);
+
+        // 取消人脸识别
+        intent.putExtra("noFaceDetection", true);
+        // 图片输出格式
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+
+        // 若为false则表示不返回数据
+        intent.putExtra("return-data", false);
+
+        // 指定裁剪完成以后的图片所保存的位置,pic info显示有延时
+        if (fromCapture) {
+            // 如果是使用拍照，那么原先的uri和最终目标的uri一致,注意这里的uri必须是Uri.fromFile生成的
+            mCutUri = Uri.fromFile(imgFile);
+        } else { // 从相册中选择，那么裁剪的图片保存在take_photo中
+            String time = new SimpleDateFormat("yyyyMMddHHmmss", Locale.CHINA).format(new Date());
+            String fileName = "photo_" + time;
+            File mCutFile = new File(Environment.getExternalStorageDirectory() + "/take_photo/", fileName + ".jpeg");
+            if (!mCutFile.getParentFile().exists()) {
+                mCutFile.getParentFile().mkdirs();
+            }
+            mCutUri = Uri.fromFile(mCutFile);
+        }
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mCutUri);
+        Toast.makeText(this, "剪裁图片", Toast.LENGTH_SHORT).show();
+        // 以广播方式刷新系统相册，以便能够在相册中找到刚刚所拍摄和裁剪的照片
+        Intent intentBc = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        intentBc.setData(uri);
+        this.sendBroadcast(intentBc);
+
+        startActivityForResult(intent, REQUEST_CROP); //设置裁剪参数显示图片至ImageVie
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                // 拍照并进行裁剪
+                case REQUEST_TAKE_PHOTO:
+                    Log.e(TAG, "onActivityResult: imgUri:REQUEST_TAKE_PHOTO:" + imgUri.toString());
+                    cropPhoto(imgUri, true);
+                    break;
+
+                // 裁剪后设置图片
+                case REQUEST_CROP:
+                    img.setImageURI(mCutUri);
+                    Log.e(TAG, "onActivityResult: imgUri:REQUEST_CROP:" + mCutUri.toString());
+                    break;
+                // 打开图库获取图片并进行裁剪
+                case SCAN_OPEN_PHONE:
+                    Log.e(TAG, "onActivityResult: SCAN_OPEN_PHONE:" + data.getData().toString());
+                    cropPhoto(data.getData(), false);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    // 从file中获取uri
+    // 7.0及以上使用的uri是contentProvider content://com.rain.takephotodemo.FileProvider/images/photo_20180824173621.jpg
+    // 6.0使用的uri为file:///storage/emulated/0/take_photo/photo_20180824171132.jpg
+    private static Uri getUriForFile(Context context, File file) {
+        if (context == null || file == null) {
+            throw new NullPointerException();
+        }
+        Uri uri;
+        if (Build.VERSION.SDK_INT >= 24) {
+            uri = FileProvider.getUriForFile(context.getApplicationContext(), "com.rain.takephotodemo.FileProvider", file);
+        } else {
+            uri = Uri.fromFile(file);
+        }
+        return uri;
+    }
+}
